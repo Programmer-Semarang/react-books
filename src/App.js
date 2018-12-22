e9e0ff2a50d66ebb7d3145fcc52970ef610ce844
Ariaseta Setia Alam <07ariaseta@gmail.com>
2018-12-22 07:46:49 +0000

    udah jadi consume api get

--- src/App.js
+++ src/App.js
@@ -1,28 +1,60 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+
+  state = {
+    books: []
+  }
+
+  componentDidMount(){
+    axios.get("http://167.160.188.135/api/book")
+         .then((response)=>{
+           console.log(response.data.data)
+           this.setState({
+             books: response.data.data
+           })
+         })
+         .catch((error)=>{
+           console.error(error)
+         })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <BookList books={this.state.books} />
       </div>
     );
   }
+}
+
+class Book extends React.Component{
+
+  render(){
+    return(
+      <div>
+        <h1>{this.props.judul}</h1>
+        <h3>{this.props.author.name}</h3>
+        <hr />
+      </div>)
+  }
+
+}
+
+class BookList extends React.Component{
+
+  render(){
+    return(
+      <div>
+        {this.props.books.map((book)=>{
+          return <Book judul={book.judul} author={book.author} />
+        })}
+      </div>)
+  }
+
 }
 
 export default App;
